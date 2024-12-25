6d714a842ce1bd900ca877ae0917e63a6a5ffd04
Yotham-Sameh <yothamsameh5@gmail.com>
2024-12-25 20:43:19 +0200

    Add files via upload

---
+++ Assign 1 cSharp/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Assign_1_cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Question 1
+            //Console.WriteLine("Enter a number:");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"You entered: {userInput}");
+            //#endregion
+
+            //#region Question 2
+            //string nonNumericString = "246"; 
+            //try
+            //{
+            //    int result = Convert.ToInt32(nonNumericString);
+            //    Console.WriteLine($"Converted result: {result}");
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+            //#endregion
+
+            //#region Question 3
+            //float a = 4.5f, b = 3.2f;
+            //float sum = a + b;
+            //Console.WriteLine($"Sum of {a} and {b} is: {sum}");
+            //#endregion
+
+            //#region Question 4
+            //string mainString = "Hello, World!";
+            //string substring = mainString.Substring(8, 4);
+            //Console.WriteLine($"Extracted substring: {substring}");
+            //#endregion
+
+            //#region Question 5
+            //int x = 5, y = x;
+            //y = 10;
+            //Console.WriteLine($"Original value: {x}, Modified value: {y}");
+            //#endregion
+
+            //#region Question 6
+            //int[] arr1 = { 2, 4, 6 };
+            //int[] arr2 = arr1;
+            //arr2[0] = 100;
+            //Console.WriteLine($"arr1[0]: {arr1[0]}, arr2[0]: {arr2[0]}");
+            //#endregion
+
+            //#region Question 7
+            //string str1 = "Hello";
+            //string str2 = "World";
+            //string combinedString = str1 + " " + str2;
+            //Console.WriteLine($"Combined string: {combinedString}");
+            //#endregion
+
+            //#region Question 8
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine($"Value of d: {d}");
+            //#endregion
+
+            //#region Question 9
+            //Console.WriteLine($"Output: {13 / 2} {13 % 2}");
+            //#endregion
+
+            //#region Question 10
+            //int num = 1, z = 5;
+            //if (!(num <= 0))
+            //    Console.WriteLine($"{++num + z++} {++z}");
+            //else
+            //    Console.WriteLine($"{--num + z--} {--z}");
+            //#endregion
+        }
+    }
+}
